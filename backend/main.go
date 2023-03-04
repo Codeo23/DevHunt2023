0b5beff99c7c2f0a0cf140ef12a033871f99a5fa
TianaNanta <andrianjakananta@gmail.com>
2023-03-04 10:28:41 +0300

    setup router

--- backend/main.go
+++ backend/main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"github.com/Codeo23/DevHunt2023/backend/config"
+	"github.com/Codeo23/DevHunt2023/backend/router"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(
+		fiber.Config{
+			CaseSensitive: true,
+			AppName:       "DevHunt2023",
+		},
+	)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	// Setup routes
+	router.SetupRoutes(app)
 
 	app.Listen(config.Config("PORT"))
 }
