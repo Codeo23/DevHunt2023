65cdf297dce854d0e5ea22a46c6d8c80efe9292d
TianaNanta <andrianjakananta@gmail.com>
2023-03-04 13:28:54 +0300

    hash password func

--- backend/controllers/user.go
+++ backend/controllers/user.go
@@ -1,1 +1,11 @@
 package controllers
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+// hash password
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
