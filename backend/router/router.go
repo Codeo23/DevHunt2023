ec554af7324ca347aab6b09aebd27d9220e44c9c
TianaNanta <andrianjakananta@gmail.com>
2023-03-04 10:28:25 +0300

    add router

---
+++ backend/router/router.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"github.com/Codeo23/DevHunt2023/backend/controllers"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+// SetupRoutes sets up the routes
+func SetupRoutes(app *fiber.App) {
+	app.Use(logger.New())
+	api := app.Group("/api", logger.New())
+	api.Get("/", controllers.Hello)
+
+	// user routes
+	user := api.Group("/user")
+	user.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, User!")
+	})
+}
