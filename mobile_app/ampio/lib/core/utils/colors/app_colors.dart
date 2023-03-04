83744a57895cf5040a5fce3c403bf8eae0d74639
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2023-03-04 21:19:23 +0300

    [ADD] new color

--- mobile_app/ampio/lib/core/utils/colors/app_colors.dart
+++ mobile_app/ampio/lib/core/utils/colors/app_colors.dart
@@ -9,5 +9,6 @@ abstract class AppColors {
   static Color yellowPrimary = const Color.fromRGBO(255, 177, 45, 1);
   static Color orangePrimary = const Color.fromRGBO(255,177,45,1);
   static Color greyPrimary = const Color.fromRGBO(231, 231, 231, 1);
+  static Color greySecondary = const Color.fromRGBO(56, 56, 56, 1);
   static Color darkPrimary = const Color.fromRGBO(30, 30, 30, 1);
 }
