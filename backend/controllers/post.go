03a0be3beaf2f6b0644a4ab220a4a5f632454a7c
TianaNanta <andrianjakananta@gmail.com>
2023-03-04 17:36:36 +0300

    now, it is possible to upload file in posts

--- backend/controllers/post.go
+++ backend/controllers/post.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"fmt"
+
 	"github.com/Codeo23/DevHunt2023/backend/database"
 	"github.com/Codeo23/DevHunt2023/backend/models"
 
@@ -31,6 +33,7 @@ func Publish(c *fiber.Ctx) error {
 
 	// get body
 	type PublishRequest struct {
+		ID      uint   `json:"id"`
 		Title   string `json:"title"`
 		Content string `json:"content"`
 	}
@@ -44,11 +47,30 @@ func Publish(c *fiber.Ctx) error {
 	// database
 	db := database.Database.DB
 
+	// upload file with post
+	file, er := c.FormFile("file")
+	if er != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid file",
+		})
+	}
+
+	fileName := fmt.Sprintf("%d_%d_%s", body.ID, user_id, file.Filename)
+	link := fmt.Sprintf("./public/posts/%s", fileName)
+
+	// upload file
+	if err := c.SaveFile(file, link); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Error while uploading file",
+		})
+	}
+
 	// create post
 	post := models.Post{
 		AuthorID: uint(user_id),
 		Title:    body.Title,
 		Content:  body.Content,
+		File:     link,
 	}
 	if err := db.Create(&post).Error; err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
