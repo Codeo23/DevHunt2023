076e0c2fe79e10bd73db604adfa1d5a994cd5623
TianaNanta <andrianjakananta@gmail.com>
2023-03-04 18:38:27 +0300

    update comment model

--- backend/models/comment.go
+++ backend/models/comment.go
@@ -8,7 +8,7 @@ import (
 type Comment struct {
 	gorm.Model
 	Content  string `json:"content"`
+	File     string `json:"file"`
 	AuthorID uint   `json:"author_id" gorm:"not null; index; references:ID;"` // foreign key
 	PostID   uint   `json:"post_id"`
-	File     string `json:"file"`
 }
