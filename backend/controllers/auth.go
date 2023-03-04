c85ae1acaed0fdcd2d5783ebfff9a9215ba1af2a
TianaNanta <andrianjakananta@gmail.com>
2023-03-04 12:01:29 +0300

    get user by email func

--- backend/controllers/auth.go
+++ backend/controllers/auth.go
@@ -1,6 +1,13 @@
 package controllers
 
 import (
+	"errors"
+
+	"github.com/Codeo23/DevHunt2023/backend/database"
+	"github.com/Codeo23/DevHunt2023/backend/models"
+
+	"gorm.io/gorm"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -8,4 +15,16 @@ import (
 func CheckPasswordHash(password string, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+func getUserByEmail(e string) (*models.User, error) {
+	db := database.Database.DB
+	var user models.User
+	if err := db.Where(&models.User{Email: e}).Find(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
 }
