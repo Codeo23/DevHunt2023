30c033a0f41168d2ca34e77324fbdfc93fef1b1f
ROBUSTE Manohisafidy <robustemmanuel@gmail.com>
2023-03-04 11:27:41 +0300

    [ADD] path login screen

--- mobile_app/ampio/lib/core/router/app_router.dart
+++ mobile_app/ampio/lib/core/router/app_router.dart
@@ -1,14 +1,21 @@
+import 'package:ampio/features/login/presentation/login_screen.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../core/utils/constants/route_path.dart';
 import '../../../features/welcome/presentation/welcome_screen.dart';
 
 abstract class AppRouter {
-  static GoRouter configurations =
-      GoRouter(initialLocation: RoutePath.welcomePath, routes: [
-    GoRoute(
-      path: RoutePath.welcomePath,
-      builder: (context, state) => const WelcomeScreen(),
-    ),
-  ]);
+  static GoRouter configurations = GoRouter(
+    initialLocation: RoutePath.loginPath,
+    routes: [
+      GoRoute(
+        path: RoutePath.welcomePath,
+        builder: (context, state) => const WelcomeScreen(),
+      ),
+      GoRoute(
+        path: RoutePath.loginPath,
+        builder: (context, state) => const LoginScreen(),
+      ),
+    ],
+  );
 }
