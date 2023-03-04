5f708e00e51448d449315d273f1a0ccec65251e7
TianaNanta <andrianjakananta@gmail.com>
2023-03-04 10:18:55 +0300

    create database connexion

---
+++ backend/database/database.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Codeo23/DevHunt2023/backend/config"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+// Db instance
+type DbInstance struct {
+	DB *gorm.DB
+}
+
+// DbInstance instance
+var Database DbInstance
+
+// Connect to the database
+func Connect() {
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_HOST"), config.Config("DB_PORT"), config.Config("DB_NAME"))
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting to database: ", err)
+	}
+
+	log.Println("Database connected successfully")
+	db.Logger = logger.Default.LogMode(logger.Info)
+
+	Database = DbInstance{DB: db}
+}
