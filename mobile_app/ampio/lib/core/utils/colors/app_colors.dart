f230928ef2751ead9e0ba485501eb83263b807cd
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2023-03-04 12:00:43 +0300

    [ADD] new color

--- mobile_app/ampio/lib/core/utils/colors/app_colors.dart
+++ mobile_app/ampio/lib/core/utils/colors/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/painting.dart';
 abstract class AppColors {
   static Color greenPrimary = const Color.fromRGBO(6, 70, 53, 1);
   static Color greenSecondary = const Color.fromRGBO(3, 201, 136, 1);
+  static Color bluePrimary = const Color.fromRGBO(19, 0, 90, 1);
 }
