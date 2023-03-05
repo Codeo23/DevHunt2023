c7bcd387ecadf6998230039f035a35f6a513a698
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2023-03-05 16:08:51 +0300

    [DONE] user authentication with firebase

--- mobile_app/ampio/lib/core/domain/data/remote/repository/auth_repository.dart
+++ mobile_app/ampio/lib/core/domain/data/remote/repository/auth_repository.dart
@@ -1,29 +1,14 @@
-import 'package:dio/dio.dart';
-
-import '../../../../../core/domain/data/remote/network_service.dart';
-import '../../../../../core/utils/constants/api_end_point.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthRepository {
 
-  final _networkService = NetworkService();
+  Future<UserCredential> login(String username, String password) async {
 
-  Future<Response> login(String username, String password) async {
-    final response = await _networkService.post(
-      path: ApiEndPoint.authentication,
-      data: {
-        'identity': username,
-        'password': password,
-      },
+    final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: username,
+      password: password,
     );
 
-    return response;
-  }
-
-  Future<Response> getCurrentUser() async {
-    final response = await _networkService.get(
-      path: ApiEndPoint.currentUser,
-    );
-
-    return response;
+    return credential;
   }
 }
