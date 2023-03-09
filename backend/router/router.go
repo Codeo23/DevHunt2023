6964a7907ea70b08895a90bc27eb4a80e33838a1
TianaNanta <andrianjakananta@gmail.com>
2023-03-09 23:15:53 +0300

    reaction endpoint

--- backend/router/router.go
+++ backend/router/router.go
@@ -10,6 +10,7 @@ import (
 // SetupRoutes sets up the routes
 func SetupRoutes(app *fiber.App) {
 	app.Use(logger.New())
+	app.Static("/", "./static/public")
 	api := app.Group("/api", logger.New())
 	api.Get("/", handlers.Hello)
 
@@ -47,6 +48,7 @@ func SetupRoutes(app *fiber.App) {
 	comment.Get("/:post_id<int>", handlers.GetComments)
 
 	comment.Post("/:post_id<int>/comment", middleware.Protected(), handlers.Comment)
+	comment.Post("/:post_id<int>/comment/:id<int>/react", middleware.Protected(), handlers.ReactToComment)
 
 	comment.Delete("/user_:user_id<int>/:id<int>", middleware.Protected(), handlers.DeleteComment)
 
